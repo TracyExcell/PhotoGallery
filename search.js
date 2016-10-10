29db09d7050ffc676bef85896b7543a8cce237a8
TracyExcell <excelltracy@gmail.com>
2016-10-10 13:00:00 +1300

    Photo Gallery Assignment First Attempt
    Photo Gallery Assignment from Tree House

---
+++ search.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  
+    $("#search").keyup(function(){
+        
+        var current_query = $("#search").val();
+        
+        if(current_query != ""){
+         $("#photo-container li").hide();
+            
+         $("#photo-container li ").each(function(){
+           
+             var current_keyword = $(this).attr("data-keywords");
+            
+            if (current_keyword.indexOf(current_query) >= 0){
+                $(this).show();
+            } 
+              });
+        } else {
+                $("#photo-container li ").show();
+            }
+               
+    });  
+    
+});
